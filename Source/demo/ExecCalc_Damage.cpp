5f9001f63cec2dfb321451e39da4ad7ad7ed075d
Lil-Pebble-X <283172628@qq.com>
2026-07-10 03:02:08 +0800

    #ExecCalc_Damage1

--- Source/demo/ExecCalc_Damage.cpp
+++ Source/demo/ExecCalc_Damage.cpp
@@ -2,4 +2,51 @@
 
 
 #include "ExecCalc_Damage.h"
+#include "AbilitySystemComponent.h"
+#include "BaseAttributeSet.h"
 
+struct ShooterDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Shield);
+
+	ShooterDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Shield, Target, false);
+	}
+};
+
+static const ShooterDamageStatics& DamageStatics()
+{
+	static ShooterDamageStatics DStatics;
+	return DStatics;
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ShieldDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+	
+	const AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Shield = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ShieldDef,	EvaluationParameters, Shield);
+	Shield = FMath::Max<float>(0.f, Shield);
+	--Shield;
+
+	const FGameplayModifierEvaluatedData EvaluatedDta(DamageStatics().ShieldProperty, EGameplayModOp::Additive, Shield);
+	OutExecutionOutput.AddOutputModifier(EvaluatedDta);
+}
