27accce765d409b7e71487a7911d26ada7d62aaa
Lil-Pebble-X <283172628@qq.com>
2026-07-11 04:25:35 +0800

    #CritDamage

--- Source/demo/ExecCalc_Damage.cpp
+++ Source/demo/ExecCalc_Damage.cpp
@@ -4,14 +4,19 @@
 #include "ExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
 #include "BaseAttributeSet.h"
+#include "ShooterGameplayTags.h"
 
 struct ShooterDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Shield);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CritRate);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CritDmg);
 
 	ShooterDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Shield, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, CritRate, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, CritDmg, Target, false);
 	}
 };
 
@@ -24,6 +29,8 @@ static const ShooterDamageStatics& DamageStatics()
 UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ShieldDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CritRateDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CritDmgDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -42,11 +49,29 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
 
-	float Shield = 0.f;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ShieldDef,	EvaluationParameters, Shield);
-	Shield = FMath::Max<float>(0.f, Shield);
-	--Shield;
+	// Get Damage Set by Caller Magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FShooterGameplayTags::Get().Damage);
 
-	const FGameplayModifierEvaluatedData EvaluatedDta(DamageStatics().ShieldProperty, EGameplayModOp::Additive, Shield);
+	// Check critical hit chance and critical damage multiplier; determine if a critical hit occurs.
+	// If critical, increase damage by the critical damage multiplier.
+
+	float TargetCritRate = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CritRateDef, EvaluationParameters, TargetCritRate);
+	TargetCritRate = FMath::Max<float>(TargetCritRate, 0.f);
+
+	float TargetCritDmg = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CritDmgDef, EvaluationParameters, TargetCritDmg);
+	TargetCritDmg = FMath::Max<float>(TargetCritDmg, 0.f);
+
+	const bool bCriticalHit = FMath::RandRange(0, 99) <TargetCritRate;
+	if (bCriticalHit)
+	{
+		Damage = (TargetCritDmg / 100.f) * Damage;
+	}
+
+	// Detect Shield; if present, absorb damage with the shield.
+
+
+	const FGameplayModifierEvaluatedData EvaluatedDta(UBaseAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedDta);
 }
